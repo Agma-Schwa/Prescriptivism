36bb800432eb4a4c0de4ad30041cb6072d5fdd85
Annwan <annwan@annwan.me>
2024-11-07 00:33:15 +0100

    [Networking] Password and port options on the server; password checking (#3)

--- src/Server/Main.cc
+++ src/Server/Main.cc
@@ -1,6 +1,26 @@
+#include <clopts.hh>
+#include <print>
+
 import pr.server;
 import pr.tcp;
+import pr.utils;
 
-int main() {
-    pr::server::Server(pr::net::DefaultPort).Run();
+using namespace pr;
+using namespace command_line_options;
+
+using options = clopts<
+        option<"--port", "The port to listen too", i64>,
+        option<"--pwd", "Password to the game">,
+        help<>
+    >;
+
+int main(int argc, char* argv[]) {
+    auto opts = options::parse(argc, argv);
+    i64 port = opts.get_or<"--port">(net::DefaultPort);
+    if (port <= 0 or port > std::numeric_limits<u16>::max()) {
+        std::println(stderr,"ERROR: invalid port {}", port);
+        return 1;
+    }
+    auto pwd = opts.get_or<"--pwd">("");
+    server::Server(u16(port), std::move(pwd)).Run();
 }
