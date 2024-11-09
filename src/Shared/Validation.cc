8a5292fe12a9388ff678442b60a7a2355a21dbdf
Annwan <annwan@annwan.me>
2024-11-09 02:19:17 +0100

    [Validation] Use an enum for validation results (#12)

--- src/Shared/Validation.cc
+++ src/Shared/Validation.cc
@@ -7,7 +7,8 @@ import pr.utils;
 import pr.cards;
 
 
-bool pr::validation::ValidateInitialWord(constants::Word word, constants::Word original) {
+auto pr::validation::ValidateInitialWord(constants::Word word, constants::Word original)
+    -> InitialWordValidationResult {
     static auto IsConsonant = [](CardId id) -> bool {
         return CardDatabase[+id].is_consonant();
     };
@@ -15,7 +16,7 @@ bool pr::validation::ValidateInitialWord(constants::Word word, constants::Word o
     rgs::sort(original);
     constants::Word w2 = word;
     rgs::sort(w2);
-    if (w2 != original) return false;
+    if (w2 != original) return InitialWordValidationResult::NotAPermutation;
 
     // No cluster or hiatus shall be longer than 2 sounds
     if (rgs::any_of( // clang-format off
@@ -23,18 +24,19 @@ bool pr::validation::ValidateInitialWord(constants::Word word, constants::Word o
             return IsConsonant(a) == IsConsonant(b);
         }),
         [](auto x) { return rgs::distance(x) > 2; })
-    ) return false; // clang-format on
+    ) return InitialWordValidationResult::ClusterTooLong; // clang-format on
 
     // M1 and M2 CANNOT start a consonant cluster word-initially.
-    if(IsConsonant(word[0]) and IsConsonant(word[1]) and CardDatabase[+word[0]].manner_or_height <= 2) return false;
+    if(IsConsonant(word[0]) and IsConsonant(word[1]) and CardDatabase[+word[0]].manner_or_height <= 2)
+        return InitialWordValidationResult::BadInitialClusterManner;
 
     // Two consonants with the same coordinates CANNOT cluster word-initially.
     if (
         IsConsonant(word[0]) and IsConsonant(word[1]) and
         CardDatabase[+word[0]].manner_or_height == CardDatabase[+word[1]].manner_or_height and
         CardDatabase[+word[0]].place_or_frontness == CardDatabase[+word[1]].place_or_frontness
-    ) return false;
+    ) return InitialWordValidationResult::BadInitialClusterCoordinates;
 
     // Else all seems good
-    return true;
+    return InitialWordValidationResult::Valid;
 }
