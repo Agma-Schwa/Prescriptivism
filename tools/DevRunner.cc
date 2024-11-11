ebecbaa83fded7732ea02e2f25c9572a8d380a2c
Sirraide <aeternalmail@gmail.com>
2024-11-11 01:43:24 +0100

    [Tools] Add runner

---
+++ tools/DevRunner.cc
@@ -0,0 +1,82 @@
+#include <print>
+#include <sys/wait.h>
+
+#ifndef __linux__
+int main() {
+    std::println(stderr, "Sorry, not supported on this platform");
+    return 1;
+}
+#else
+#    include <csignal>
+#    include <unistd.h>
+#    include <utility>
+
+[[noreturn]] void Kill(int = 0) {
+    killpg(0, SIGKILL);
+    _Exit(42);
+}
+
+int main() {
+    setpgid(0, 0);
+
+    std::atexit([] { Kill(); });
+    signal(SIGINT, Kill);
+    signal(SIGTERM, Kill);
+    signal(SIGHUP, Kill);
+    signal(SIGQUIT, Kill);
+    signal(SIGSEGV, Kill);
+    signal(SIGABRT, Kill);
+    signal(SIGPIPE, SIG_IGN);
+    signal(SIGCHLD, SIG_IGN);
+
+    // Do not rebuild ourselves as that may cause a crash.
+    std::system("cmake --build out -- PrescriptivismServer Prescriptivism");
+
+    if (fork() == 0) {
+        execl(
+            "./PrescriptivismServer",
+            "PrescriptivismServer",
+            "--pwd",
+            "password",
+            nullptr
+        );
+        abort();
+    }
+
+    if (fork() == 0) {
+        execl(
+            "./Prescriptivism",
+            "Prescriptivism",
+            "--connect",
+            "localhost",
+            "--name",
+            "testuser1",
+            "--password",
+            "password",
+            nullptr
+        );
+        abort();
+    }
+
+    if (fork() == 0) {
+        execl(
+            "./Prescriptivism",
+            "Prescriptivism",
+            "--connect",
+            "localhost",
+            "--name",
+            "testuser2",
+            "--password",
+            "password",
+            nullptr
+        );
+        abort();
+    }
+
+    waitpid(0, nullptr, 0);
+    waitpid(0, nullptr, 0);
+    waitpid(0, nullptr, 0);
+    std::println("All children have exited normally");
+    return 0;
+}
+#endif
